77a90bfa446bf6b51189f6317a7b70a1493499fd
Josh Lear <joshrlear@gmail.com>
2019-09-04 14:47:11 -0700

    add sockets

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
-//import io from 'socket.io-client'
 import config from '../config'
+import io from 'socket.io-client'
 //import UdownContext from '../UdownContext'
 import ScrollToBottom from 'react-scroll-to-bottom'
 import Textarea from 'react-textarea-autosize'
 import './Chat.css'
 
-//let socket
+// on render, client connects to socket.io via server
+const socket = io.connect(config.SERVER_BASE_URL)
 
 export default class Chat extends Component {
     constructor(props) {
@@ -14,22 +15,55 @@ export default class Chat extends Component {
         this.chatInput = React.createRef()
         this.chatSection = React.createRef()
         this.messagesEnd = React.createRef()
+        this.chatRoom = React.createRef()
         this.state = {
             currentMessage: '',
             messages: [],
             userTyping: [],
+            chatRoom: '',
         }
     }
 
     //static contextType = UdownContext
 
     componentWillMount() {
+
+        // connects to chat namespace 
+        let chat = io(`${config.SERVER_BASE_URL}chat`)
+        // listens for welcome message then logs to console
+        chat.on('welcome', msg => {
+            console.log(msg)
+        })
+
+        // client listens for 'news' socket then
+        // logs the data emitted from 'news socket
+        socket.on('news', data => {
+            console.log(data);
+
+            // client then emits 'my other event' socket
+            // and sends object my: data to server
+            socket.emit('my other event', { my: 'data' });
+          })
+         
+        // client listens for 'incoming message' socket connection
+        // then it logs the newMsg  
+        socket.on('incoming message', newMsg => {
+            console.log(newMsg)
+        })
+
         // connect to socket if no socket connected
         //!socket && (this.connectSocket())
     }
 
     componentDidUpdate() {
         this.messagesEnd.current && this.messagesEnd.current.scrollIntoView({ behavior: "smooth" })
+    }
+
+    componentWillUnmount() {
+
+        // client emits 'disconnected' socket
+        // when they leave the chat
+        socket.emit('disconnected', localStorage.username)
     }
 
     /* connectSocket() {
@@ -73,6 +107,16 @@ export default class Chat extends Component {
         //this.context.closeChat()
     }
 
+    handleChatRoom = e => {
+        e.preventDefault()
+        const chatRoom = this.chatRoom
+        
+        this.setState({
+            chatRoom
+        })
+        socket.emit('joinRoom', chatRoom)
+    }
+
     handleInput = (message) => {
         console.log('message here:',message)
         
@@ -87,7 +131,7 @@ export default class Chat extends Component {
         })
     }
 
-    sendMessage= e => {
+    sendMessage = e => {
         e.preventDefault()
         const newMsg = this.state.currentMessage
         const { username, message } = this.state.currentMessage
@@ -97,6 +141,7 @@ export default class Chat extends Component {
         })
         //socket.emit('chat_message', ({ username: username, message: message}))
         this.chatInput.current.value = ''
+        socket.emit('newMessage', newMsg )
         this.setState({
             currentMessage: ''
         })
@@ -107,6 +152,8 @@ export default class Chat extends Component {
         return(
         <>
             <div className={ chatToggle }>
+                <input ref={ this.chatRoom }/>
+                <button onClick={ this.handleChatRoom }>join room</button>
                 <div className="chat_title_container">
                 
                     {/* <button id="close_btn" className="close_btn" href="javascript:void(0)" onClick={e => this.closeChat(e)}><i className="fas fa-times fa-2x"></i></button> */}
