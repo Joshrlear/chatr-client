bdf9ba5e4909af530d617daa2f082375b008ce87
Josh Lear <joshrlear@gmail.com>
2019-09-03 19:33:14 -0700

    add test, deploy

--- src/Chat/Chat.test.js
+++ src/Chat/Chat.test.js
@@ -1,5 +1,15 @@
-const tree = renderer
-  .create(<Textarea />, {
-    createNodeMock: () => document.createElement('textarea')
-  })
-  .toJSON();
+import React from 'react';
+//import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+//import Chat from './Chat';
+import Textarea from 'react-textarea-autosize';
+
+describe('Chat rendering', () => {
+  it('renders Textarea properly', () => {
+    const tree = renderer
+      .create(<Textarea />, {
+        createNodeMock: () => document.createElement('textarea')
+      })
+      .toJSON();
+      })
+});
