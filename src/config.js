5706ea93ef9a1266367dffe8488882ecf34334c7
Josh Lear <joshrlear@gmail.com>
2019-09-06 23:20:27 -0700

    .

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-    SERVER_BASE_URL: /* "https://secure-brushlands-32641.herokuapp.com/" */ "http://localhost:5000/"
+    SERVER_BASE_URL: "https://secure-brushlands-32641.herokuapp.com/" /* "http://localhost:5000/" */
 }
