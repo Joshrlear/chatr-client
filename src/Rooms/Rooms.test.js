446e231f8e866c41756b5ce37801e29837bb12b3
Josh Lear <joshrlear@gmail.com>
2019-09-15 10:46:50 -0700

    .

--- src/Rooms/Rooms.test.js
+++ src/Rooms/Rooms.test.js
@@ -1,20 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import Rooms from './Rooms';
 import renderer from 'react-test-renderer';
 import Textarea from 'react-textarea-autosize';
-
-describe('Components', () => {
-    it(`renders Rooms without crashing`, () => {
-        const div = document.createElement('div')
-        ReactDOM.render(
-        <BrowserRouter>
-           <Rooms/>
-        </BrowserRouter>, div)
-        ReactDOM.unmountComponentAtNode(div)
-    })
-})
 
 describe('Textarea rendering', () => {
   it('renders Textarea properly', () => {
